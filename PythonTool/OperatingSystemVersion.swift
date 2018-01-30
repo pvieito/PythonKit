65fbae5ccf54f76e77cdcfa50e92d967fe69203c
Pedro José Pereira Vieito <pvieito@gmail.com>
2018-01-30 01:01:41 +0100

    Initial Commit

---
+++ PythonTool/OperatingSystemVersion.swift
@@ -0,0 +1,16 @@
+//
+//  Ope.swift
+//  PythonTool
+//
+//  Created by Pedro José Pereira Vieito on 29/1/18.
+//  Copyright © 2018 Pedro José Pereira Vieito. All rights reserved.
+//
+
+import Foundation
+
+extension OperatingSystemVersion: CustomStringConvertible {
+    
+    public var description: String {
+        return "\(self.majorVersion).\(self.minorVersion).\(self.patchVersion)"
+    }
+}
---
+++ PythonTool/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  PythonTool
+//
+//  Created by Pedro José Pereira Vieito on 29/1/18.
+//  Copyright © 2018 Pedro José Pereira Vieito. All rights reserved.
+//
+
+
+import Foundation
+import LoggerKit
+import CommandLineKit
+import PythonKit
+
+let listOption = BoolOption(shortFlag: "l", longFlag: "list", helpMessage: "List installed modules.")
+let verboseOption = BoolOption(shortFlag: "v", longFlag: "verbose", helpMessage: "Verbose mode.")
+let helpOption = BoolOption(shortFlag: "h", longFlag: "help", helpMessage: "Prints a help message.")
+
+let cli = CommandLineKit.CommandLine()
+cli.addOptions(listOption, verboseOption, helpOption)
+
+do {
+    try cli.parse()
+}
+catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if helpOption.value {
+    cli.printUsage()
+    exit(-1)
+}
+
+Logger.logMode = .commandLine
+Logger.logLevel = verboseOption.value ? .debug : .info
+
+let sysModule = Python.import("sys")
+
+let sysPath = sysModule.get(member: "path")
+
+sysPath.call(member: "insert", args: 0, "/usr/local/lib/python2.7/site-packages")
+
+let pipModule = Python.import("pip")
+
+let pythonVersionInfo = sysModule.get(member: "version_info")
+let pythonVersion =
+OperatingSystemVersion(majorVersion: Int(pythonVersionInfo.get(member: "major"))!,
+                       minorVersion: Int(pythonVersionInfo.get(member: "minor"))!,
+                       patchVersion: Int(pythonVersionInfo.get(member: "micro"))!)
+
+Logger.log(important: "PythonKit")
+Logger.log(info: "Version: \(pythonVersion)")
+Logger.log(verbose: "Version String:\n\(sysModule.get(member: "version"))")
+
+let installedModules = pipModule.call(member: "get_installed_distributions")
+
+if listOption.value, installedModules.count > 0 {
+    Logger.log(important: "Installed Modules (\(installedModules.count)):")
+    
+    for installedModule in installedModules {
+        Logger.log(success: installedModule)
+    }
+}
