686d474fa9c2df3dd17ecd0400aeafc9631385c8
Pedro José Pereira Vieito <pvieito@gmail.com>
2018-08-13 23:22:38 +0200

    Fixed access level

--- PythonKit/PythonGlue+Python3.swift
+++ PythonKit/PythonGlue+Python3.swift
@@ -9,23 +9,23 @@
 #if canImport(Python3) && !PYTHON2
 import Python3
 
-private let PyInt_AsLong = PyLong_AsLong
-private let PyInt_AsUnsignedLongMask = PyLong_AsUnsignedLongMask
-private let PyString_AsString = PyUnicode_AsUTF8
+internal let PyInt_AsLong = PyLong_AsLong
+internal let PyInt_AsUnsignedLongMask = PyLong_AsUnsignedLongMask
+internal let PyString_AsString = PyUnicode_AsUTF8
 
-private func PyInt_FromLong(_ int: Int) -> UnsafeMutablePointer<PyObject> {
+internal func PyInt_FromLong(_ int: Int) -> UnsafeMutablePointer<PyObject> {
     return PyLong_FromLong(int)
 }
 
-private let PyInt_FromSize_t = PyInt_FromLong
+internal let PyInt_FromSize_t = PyInt_FromLong
 
-private var _Py_ZeroStruct = _Py_FalseStruct
+internal var _Py_ZeroStruct = _Py_FalseStruct
 
-private func PyString_FromStringAndSize(_ string: UnsafePointer<Int8>!, _ size: Py_ssize_t) -> UnsafeMutablePointer<PyObject>? {
+internal func PyString_FromStringAndSize(_ string: UnsafePointer<Int8>!, _ size: Py_ssize_t) -> UnsafeMutablePointer<PyObject>? {
     return PyUnicode_DecodeUTF8(string, size, nil)
 }
 
-private func PySys_SetPath(_ path: UnsafePointer<CChar>!) {
+internal func PySys_SetPath(_ path: UnsafePointer<CChar>!) {
     let capacity = 4096
     let widecharPath = UnsafeMutableBufferPointer<wchar_t>.allocate(capacity: capacity)
     mbstowcs(widecharPath.baseAddress, path, capacity)
