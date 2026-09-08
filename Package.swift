3535165034a1f05be5b1f2355a5b6e9a63d95bc1
Pedro José Pereira Vieito <pvieito@gmail.com>
2026-09-09 01:25:34 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "PythonKit",
-    platforms: [
-        .macOS(.v26),
-    ],
     products: [
         .library(
             name: "PythonKit",
